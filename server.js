743fa13c7ac6a09860f59ac6302727fb9a6db10b
Jimmy Nguyen <jnguyen10@yahoo.com>
2016-02-11 17:06:27 -0800

    minor change, removed https redirect in server.js

--- server.js
+++ server.js
@@ -17,21 +17,6 @@ var app = express();
 // Path for Static Content
 app.use(express.static(path.join(__dirname, './client/static')));
 
-// ##### HTTPS REDIRECT #####
-// Enable reverse proxy support in Express. This causes the "X-Forwarded-Proto" header field to be trusted so its value can be used to determine the protocol.
-app.enable('trust proxy')
-// Add a handler to inspect the req.secure flag.  This allows us to know whether the request was via http or https.
-app.use (function (req, res, next) {
-    console.log("req.secure", req.secure)
-    if (req.secure) {
-        // req was via https, so do no special handling
-        next();
-    } else {
-        // req was via http, so redirect to https
-        res.redirect('https://' + req.headers.host + req.url);
-    }
-})
-
 
 // require the mongoose config file to load DB
 // NOTE: ALWAYS load before routes.js
