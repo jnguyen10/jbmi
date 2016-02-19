383530f086cc04206865ab10fa7256d0d2a1970a
Jimmy Nguyen <jnguyen10@yahoo.com>
2016-02-19 14:10:17 -0800

    updated email HTML template

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Order = mongoose.model('Order');
+// JBMI_Customer_Receipt API Key
 var sendgrid = require('sendgrid')('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')
 
 module.exports = (function(){
@@ -67,7 +68,7 @@ module.exports = (function(){
 					var total_items = req.body.order_items; 
 
 					for (each in total_items) {
-						html_string = html_string + "<tr><td>" + total_items[each]._name + "</td> <td>" + total_items[each]._quantity + " </td> <td>$" + total_items[each]._price.toFixed(2) + "</td></tr>"
+						html_string = html_string + "<tr><td>" + total_items[each]._name + "</td> <td align='center'>" + total_items[each]._quantity + " </td> <td>$" + total_items[each]._price.toFixed(2) + "</td></tr>"
 					}
 
 					console.log("**** html string ***** \n", html_string)
@@ -77,7 +78,7 @@ module.exports = (function(){
 						from: 'jbmoderninserts@gmail.com',
 						bcc: 'jbmoderninserts@gmail.com',
 						subject: 'JB Modern Inserts - Order Number: #' + req.body.order_number,
-						html: "<div style='font-family: Avenir, sans-serif'><h1>Your Recent Order from JB Modern Inserts!</h1> <p>We would like to thank you for your recent purchase from JB Modern Inserts!  Below is a summary of your purchase.  Please email us back at <a href='mailto:jbmoderninserts@gmail.com'>jbmoderninserts@gmail.com</a> with any questions or comments.  Thank you and please stop by again!</p> <br> <h3>JBMI Order Summary: Order #" + req.body.order_number + " </h3><table><thead> <tr> <th>Product</th> <th>Quantity</th><th>Total</th> </tr> </thead><tbody> " + html_string + " <tr><td style='font-weight: bold'>Shipping:</td><td></td><td> $" + req.body.shipping.toFixed(2) + " </td></tr><tr> <td style='font-weight: bold'>Order Total:</td><td></td><td> $" + req.body.price.toFixed(2) + " </td> </tr> </tbody></table><br><h4>Follow Us! Tell us what you'd like to see in our next break!</h4><a href='https://www.youtube.com/channel/UCYAXdoijV2VUP-rmB-M9cnw'>YouTube</a> | <a href='https://twitter.com/jbmoderninserts'>Twitter</a> | <a href='https://www.instagram.com/jb_moderninserts/'>Instagram</a></div>"
+						html: "<div style='font-family: Avenir, sans-serif'><h1>Your Recent Order from JB Modern Inserts!</h1> <p>We would like to thank you for your recent purchase from JB Modern Inserts!  Below is a summary of your purchase.  Please respond or email us back at <a href='mailto:jbmoderninserts@gmail.com'>jbmoderninserts@gmail.com</a> with any questions or comments.  Thank you and please stop by again!</p> <br> <h3>JBMI Order Summary: Order #" + req.body.order_number + " </h3><table><thead> <tr> <th>Product</th> <th>Quantity</th><th>Total</th> </tr> </thead><tbody> " + html_string + " <tr><td align='right' style='font-weight: bold'>Shipping:</td><td></td><td> $" + req.body.shipping.toFixed(2) + " </td></tr><tr> <td align='right' style='font-weight: bold'>Order Total:</td><td></td><td style='font-weight: bold'> $" + req.body.price.toFixed(2) + " </td> </tr> </tbody></table><br><h4>Follow Us! Tell us what you'd like to see in our next break!</h4><a href='https://www.youtube.com/channel/UCYAXdoijV2VUP-rmB-M9cnw'>YouTube</a> | <a href='https://twitter.com/jbmoderninserts'>Twitter</a> | <a href='https://www.instagram.com/jb_moderninserts/'>Instagram</a></div>"
 					};
 
 					console.log("******** PAYLOAD ********** \n", payload)
